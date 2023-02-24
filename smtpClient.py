9a74fc1b7eb9e68a7123da1e8547aa4a60b35bcd
Ramin Khan <48773515+khanramin@users.noreply.github.com>
2023-02-24 17:35:08 -0500

    Update smtpClient.py

--- smtpClient.py
+++ smtpClient.py
@@ -10,7 +10,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Create socket called clientSocket and establish a TCP connection with mailserver and port
     # Fill in start
     clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect(mailserver)
+    clientSocket.connect('smtp.nyu.edu',25)
     # Fill in end
 
     recv = clientSocket.recv(1024).decode()
@@ -49,8 +49,6 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     # Send message data.
     # Fill in start
-    subject = 'Searching for the meaning of life.\r\n'
-    clientSocket.send(subject.encode())
     clientSocket.send(msg.encode())
     # Fill in end
 
